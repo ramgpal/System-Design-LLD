03de0f000bc2c206c8be1c965da02c0a9d497889
ramgpal <ramgpal912@gmail.com>
2025-09-09 12:17:39 +0530

    Design Pattern Added

---
+++ Day2/Design Patterns/Singleton/Singleton.java
@@ -0,0 +1,15 @@
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+        System.out.println("Singleton is instantiated");
+    }
+
+    public static Singleton getInstance() {
+        if(instance == null) {
+            instance = new Singleton();
+        }
+
+        return  instance;
+    }
+}
